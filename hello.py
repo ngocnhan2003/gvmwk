1e119749e1a2274f76b218b1a3264e9346960fb3
Nhan N. TRAN <20029729+ngocnhan2003@users.noreply.github.com>
2022-10-23 14:05:41 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/handler', methods=['GET', 'POST'])
+def handle_push():
+    if not request.json:
+        abort(400)
+    print("Request: {}".format(request.json))
+    return jsonify({'status': 'ok'}), 200
+
 if __name__ == '__main__':
     app.run()
